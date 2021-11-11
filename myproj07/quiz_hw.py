f6522e28725efafdb726c55a8617dfd7b5eea9a6
Suin Kim <soo.officer@gmail.com>
2021-11-11 17:39:18 +0900

    문제풀이 방법 추가

--- myproj07/quiz_hw.py
+++ myproj07/quiz_hw.py
@@ -8,11 +8,18 @@ def myfilter(filter_fn, alter_value):
             # TODO : new_args를 적절히 구성해주세요.
             # new_args는 리스트 및 튜플 자료구조가 가능합니다.
             new_args = []
+            # 1번째 방법
+            # for arg in args:
+            #     if filter_fn(arg):
+            #         new_args.append(alter_value)
+            #     else:
+            #         new_args.append(arg)
+
+            # 2번째 방법
             for arg in args:
-                if filter_fn(arg):
-                    new_args.append(alter_value)
-                else:
-                    new_args.append(arg)
+                new_args.append(
+                    alter_value if filter_fn(arg) else arg
+                )
 
             return fn(*new_args)
         return inner
