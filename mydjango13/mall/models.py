f0867e09d2e5c429449535959755fb2a00f2284b
Suin Kim <soo.officer@gmail.com>
2021-12-05 09:34:45 +0900

    mydjango13 commit 깜빡한 파일들

---
+++ mydjango13/mall/models.py
@@ -0,0 +1,18 @@
+import datetime
+from django.db import models
+
+
+class Shop(models.Model):
+    # id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    telephone = models.CharField(max_length=13)
+    # open_time = models.TextField(default=datetime.time)
+    photo = models.ImageField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        # 관련 쿼리셋에서 order_by를 지정하지 않을 때
+        # 적용될 디폴트 정렬
+        ordering = ['-id']
