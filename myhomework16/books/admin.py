b9b84715356a3500e9f40f2ce5c97eb01f676333
Suin Kim <soo.officer@gmail.com>
2021-12-06 21:59:47 +0900

    bookstore 모델의 list와 detail 페이지 구현

--- myhomework16/books/admin.py
+++ myhomework16/books/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from books.models import Bookstore
 
+
+class BookstoreAdmin(admin.ModelAdmin):
+    pass
+
+
 admin.site.register(Bookstore)
--- myhomework16/books/views.py
+++ myhomework16/books/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from books.models import Bookstore
 
-# Create your views here.
+
+def bookstore_list(request: HttpRequest) -> HttpResponse:
+    qs = Bookstore.objects.all()
+    context = {
+        "bookstore_list": qs,
+    }
+    return render(request, "books/bookstore_list.html", context)
+
+
+def bookstore_detail(request: HttpRequest, pk: int) -> HttpResponse:
+    bookstore = Bookstore.objects.get(pk=pk)
+    context = {
+        "bookstore": bookstore,
+    }
+    return render(request,"books/bookstore_detail.html", context)
+
+
+
+
