1e6ce0be9ea893828676636e272599665c48a68c
Suin Kim <soo.officer@gmail.com>
2021-12-07 21:27:56 +0900

    앱 생성 및 등록

---
+++ myhomework17/books/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class Bookstore(models.Model):
+    product = models.CharField(max_length=50, db_index=True)
+    price = models.CharField(max_length=11)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
