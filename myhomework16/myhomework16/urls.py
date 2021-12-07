df45009bcfa1a614ecd0fec4764027418a9ab96e
Suin Kim <soo.officer@gmail.com>
2021-12-07 10:01:59 +0900

    템플릿 상속 기능 활용

--- myhomework16/myhomework16/urls.py
+++ myhomework16/myhomework16/urls.py
@@ -17,9 +17,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from books.views import bookstore_list, bookstore_detail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('books/', bookstore_list),
+    path('books/<int:pk>/', bookstore_detail),
 ]
 
 urlpatterns += static(settings.MEDIA_URL,
