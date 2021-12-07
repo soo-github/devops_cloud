620e334bdc1f0cd4fbc4a2db70360294532e1525
Suin Kim <soo.officer@gmail.com>
2021-12-07 11:19:01 +0900

    앱 생성 및 등록/ 마이그레이션

---
+++ mydjango17/diary/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+
+
+class TimestampedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True  # 추상 클래스로서, DB 테이블이 생기지 않습니다.
+
+
+class Post(TimestampedModel):
+    author_name = models.CharField(max_length=20)
+    title = models.CharField(max_length=200, db_index=True)
+    content = models.TextField()
+    photo = models.ImageField(upload_to="diary/post/%Y/%m/%d")
+    tag_set = models.ManyToManyField('Tag', blank=True)
+
+    def __str__(self) -> str:
+        return self.title
+
+    class Meta:
+        verbose_name = "포스팅"
+        verbose_name_plural = "포스팅 목록"
+
+
+class Comment(TimestampedModel):
+    author_name = models.CharField(max_length=20)
+    message = models.TextField()
+
+    class Meta:
+        verbose_name = "댓글"
+        verbose_name_plural = "댓글 목록"
+
+
+class Tag(TimestampedModel):
+    name = models.CharField(max_length=200, db_index=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        verbose_name = "태그"
+        verbose_name_plural = "태그 목록"
