456fc4e13535a2846695fa4c4543798bfde91bc6
Suin Kim <soo.officer@gmail.com>
2021-12-09 10:23:12 +0900

    mydjango17을 복사하여 mydjango19를 생성

---
+++ mydjango19/diary/urls.py
@@ -0,0 +1,13 @@
+from django.conf import settings
+from django.urls import path, include
+from diary import views
+
+app_name = "diary"
+
+urlpatterns = [
+    path('', views.post_list, name="post_list"),
+    path('<int:pk>/',views.post_detail, name="post_detail"),
+    path("tags/<str:tag_name>/", views.tag_detail, name="tag_detail"),
+]
+
+
