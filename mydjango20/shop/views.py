a6e16b552f89fbb19885e933294cbac47a34d11e
Suin Kim <soo.officer@gmail.com>
2021-12-12 07:56:17 +0900

    shop_list 뷰 구현

--- mydjango20/shop/views.py
+++ mydjango20/shop/views.py
@@ -4,11 +4,22 @@ from shop.forms import ShopForm
 from shop.models import Shop
 
 
+def shop_list(request: HttpRequest) -> HttpResponse:
+    qs = Shop.objects.all()
+    return render(request, "shop/shop_list.html", {
+        "shop_list": qs,
+    })
+
+
 # /shop/100
 def shop_detail(request: HttpRequest, pk: int) -> HttpResponse:
     shop = get_object_or_404(Shop, pk=pk)
+    review_list = shop.review_set.all()
+    tag_list = shop.tag_set.all()
     return render(request, "shop/shop_detail.html", {
         "shop": shop,
+        "review_list": review_list,
+        "tag_list": tag_list,
     })
 
 
