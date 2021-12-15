9bfed7cd1931473934c5da7d7ed05d584dc00758
Suin Kim <soo.officer@gmail.com>
2021-12-15 13:14:05 +0900

    blog 앱 post_list/post_detail/post_new/post_edit 뷰 기본 구현

--- mydjango23/blog/urls.py
+++ mydjango23/blog/urls.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from django.urls import path
+from blog import views
 
 app_name = "blog"
 
-urlpatterns = []
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+    path('<int:pk>/', views.post_detail, name='post_detail'),
+    path('new/', views.post_new, name='post_new'),
+    path('<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('<int:pk>/delete/', views.post_delete, name='post_delete'),
+]
