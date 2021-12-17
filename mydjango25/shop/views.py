82870285d4901c8223edc634f0410575e7a93f08
Suin Kim <soo.officer@gmail.com>
2021-12-17 15:36:07 +0900

    review_new 뷰를 CBV로 구현

--- mydjango25/shop/views.py
+++ mydjango25/shop/views.py
@@ -1,6 +1,11 @@
-from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponse
+from django.shortcuts import redirect, get_object_or_404
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView
 
-from shop.models import Shop, Category
+from shop.models import Shop, Category, Review
+from shop.templates.shop.forms import ReviewForm
 
 
 class ShopListView(ListView):
@@ -17,3 +22,24 @@ shop_list = ShopListView.as_view()
 shop_detail = DetailView.as_view(
     model=Shop,
 )
+
+
+class ReviewCreateView(LoginRequiredMixin, CreateView):
+    model = Review
+    form_class = ReviewForm
+    success_url = reverse_lazy("shop:shop_list")
+
+    # 유효성 검사에 통과한다면...
+    def form_valid(self, form) -> HttpResponse:
+        # self.kwargs : URL Captured 값들이 사전으로 저장
+        shop_pk = self.kwargs["shop_pk"]
+        shop = get_object_or_404(Shop, pk=shop_pk)
+
+        review = form.save(commit=False)
+        review.shop = shop
+        review.user = self.request.user
+        review.save()
+        return redirect(shop)
+
+
+review_new = ReviewCreateView.as_view()
